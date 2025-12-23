45831e4d563cf3fb2c2dda6a0eac25297f004c07
allen-troo <allen@troo.earth>
2025-12-23 20:49:46 +0530

    feat: backend endpoint into env file

--- troo-web/src/lib/axiosConfig.ts
+++ troo-web/src/lib/axiosConfig.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance, CreateAxiosDefaults } from "axios";
 import axios from "axios";
 
 const axiosConfig: CreateAxiosDefaults = {
-    baseURL: 'https://troo.backend.com',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json',
